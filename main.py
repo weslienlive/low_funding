eab47c39b67172c19d31effe427414693fb71e67
unknown <weslienlive@gmail.com>
2023-10-11 10:16:47 +0200

    add Bybit perps fetch, message on sucess message

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import json
 import requests
 from dotenv import load_dotenv 
 import pandas as pd
+import ccxt
 
 load_dotenv()
-perps = [ 'BEL','SUI', '10000LADYS', '10000NFT', '1000BONK', '1000BTT', '1000FLOKI', '1000LUNC', '1000PEPE', '1000XEC', '1INCH', 'AAVE', 'ACH', 'ADA', 'AGIX', 'AGLD', 'AKRO', 'ALGO', 'ALICE', 'ALPACA', 'ALPHA', 'AMB', 'ANKR', 'ANT', 'APE', 'API3', 'APT', 'ARB', 'ARKM', 'ARK', 'ARPA', 'AR', 'ASTR', 'ATA', 'ATOM', 'AUCTION', 'AUDIO', 'AVAX', 'AXS', 'BADGER', 'BAKE', 'BAL', 'BAND', 'BAT', 'BCH', 'BEL', 'BICO', 'BLUR', 'BLZ', 'BNB', 'BNT', 'BNX', 'BOBA', 'BSV', 'BSW', 'BTC', 
-'C98', 'CEEK', 'CELO', 'CELR', 'CFX', 'CHR', 'CHZ', 'CKB', 'COMBO', 'COMP', 'DOGE', 'DOT', 'DUSK', 'DYDX', 'EDU', 'EGLD', 'ENJ', 'ENS', 'EOS', 'ETC', 'ETH', 'FET', 'FIL', 'FITFI', 'FLM', 'FLOW', 'FLR', 'FORTH', 'FTM', 'FXS', 'GALA', 'GAL', 'GFT', 'GLMR', 'GLM', 'GMT', 'GMX', 'GPT', 'GRT', 'GTC', 'HBAR', 'HFT', 'HIGH', 'HNT', 'HOOK', 'HOT', 'ICP', 'ICX', 'IDEX', 'ID', 'ILV', 'IMX', 'INJ', 'IOST', 'IOTA', 'YGG', 'ZEC', 'ZEN', 'ZIL', 'ZRX', 'CRO', 'CRV', 'CTC', 'CTK', 'CTSI', 'CVC', 'CVX', 'CYBER', 'DAR', 'DASH', 'DENT', 'DGB', 'DODO', 'IOTX', 'JASMY', 'JOE', 'JST', 'KAS', 'KAVA', 'KDA', 'KEY', 'KLAY', 'KNC', 'KSM', 'LDO', 'LEVER', 'LINA', 'LINK', 'LIT', 'LOOKS', 'LPT', 'LQTY', 'LRC', 'LTC', 'LUNA2', 'MAGIC', 'MANA', 'MASK', 'MATIC', 'MAV', 'MC', 'MDT', 'MINA', 'MKR', 'MTL', 'NEAR', 'NEO', 'NKN', 'OCEAN', 'OGN', 'OMG', 'ONE', 'ONT', 'OP', 'ORDI', 'OXT', 'PAXG', 'PENDLE', 'PEOPLE', 'PHB', 'QNT', 'QTUM', 'RAD', 'RDNT', 'REEF', 'REN', 'REQ', 'RLC', 'RNDR', 'ROSE', 'RPL', 'RSR', 'RSS3', 'RUNE', 'RVN', 'SAND', 'SCRT', 'SC', 'SEI', 'SFP', 'SHIB1000', 'SKL', 'SLP', 'SNX', 'SOL', 'SPELL', 'SSV', 'STG', 'STMX', 'STORJ', 'STX', 
-'SUI', 'SUN', 'SUSHI', 'SWEAT', 'SXP', 'THETA', 'TLM', 'TOMO', 'TRB', 'TRU', 'TRX', 'T', 'TWT', 'UMA', 'UNFI', 'UNI', 'VET', 'VGX', 'WAVES', 'WLD', 'WOO', 'XCN', 'XEM', 'XLM', 'XMR', 'XNO', 
-'XRP', 'XTZ', 'XVG', 'YFII', 'YFI', 'CORE', 'COTI', 'FRONT', 'DEFI']
+exchange = ccxt.bybit()
+exchange.load_time_difference()
+perps = []
 funding_rates = []
 TOKEN_BOT = os.getenv('TOKEN_BOT')
 CHAT_ID = os.getenv('CHAT_ID')
@@ -23,9 +23,22 @@ def send_telegram_message(message):
         response = requests.post(telegram_url, params=params)
         if response.status_code != 200:
             print(f'Failed to send message to Telegram. Error code: {response.status_code}')
+        else:
+            print('Message succesfully sent to Telegram.')
     except Exception as e:
         print(f"Error: {e}")
 
+
+def fetch_perps():
+    try:
+        # Loop through each symbol in the 'markets' list, displaying a progress bar.
+        for symbol in tqdm(markets, desc="Fetching symbols and prices..."):
+
+            # Check if the symbol ends with ':USDT'
+            if symbol.endswith(':USDT'):
+                perps.append(symbol)
+    except Exception as e:
+        print(f'Error: {e}')
 
 
 def fetch_funding_rates():
@@ -71,10 +84,6 @@ def fetch_funding_rates():
         print(f"Error {e}")
 
 
-
-
-
-
 def fetch_lowest_rates():
     prev_message = ''
 
@@ -95,12 +104,14 @@ def fetch_lowest_rates():
                 prev_message = message
                 # Send the formatted message to Telegram
                 send_telegram_message(message)
+                print(message)
     except Exception as e:
         print(f"Error {e}")
 
 
 
 while True:
+    fetch_perps()
     fetch_funding_rates()
     fetch_lowest_rates()
     print("Waiting 2 hours")
